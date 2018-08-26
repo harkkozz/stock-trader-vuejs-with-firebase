b5450433f7bc3fa8e8a678541869971a3bc5ad54
Haris Adilovic <adilovicharis94@gmail.com>
2018-08-26 16:16:43 +0200

    stock trader web app with firebase db

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <app-header></app-header>
+      </div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+export default {
+  components: {
+    appHeader: Header
+  },
+  created() {
+    this.$store.dispatch('initStocks');
+  }
+}
+</script>
+
+<style>
+  body {
+    padding: 30px;
+  }
+</style>
---
+++ src/data/stocks.js
@@ -0,0 +1,7 @@
+export default [
+    { id: 1, name: 'BMW', price: 110 },
+    { id: 1, name: 'Google', price: 200 },
+    { id: 1, name: 'Apple', price: 250 },
+    { id: 1, name: 'Twitter', price: 70 },
+    { id: 1, name: 'Facebook', price: 90 }
+]
